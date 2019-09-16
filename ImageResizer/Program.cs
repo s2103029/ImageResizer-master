1f01cb5875b566585e298c935bf6d1d9b8979b5b
Jamie <s2103029@gmail.com>
2019-09-16 09:37:59 +0800

    1.非同步第一版修改

---
+++ ImageResizer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ImageResizer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
+            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
+
+            ImageProcess imageProcess = new ImageProcess();
+
+            imageProcess.Clean(destinationPath);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            await imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
+            sw.Stop();
+
+            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
